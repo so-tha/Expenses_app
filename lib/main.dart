6caf4ce10eed7626f97fc38e48c5e985e955d260
so-tha <thais3225@gmail.com>
2024-07-28 10:08:43 -0300

    mod: mapeamento dos objetos na lista de transações e conversão usando map

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,12 @@ class Myhomepage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
+          Column(
+            children: _transactions.map((tr){
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           )
         ],
       ),
